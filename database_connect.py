6ef0b012402953d22ff2f82e0e72a85126610c04
catalina <trainee.catalina.creanga@sigmalabs.co.uk>
2025-09-05 15:26:02 +0100

    connecting to csv file

---
+++ database_connect.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+# Connect to database
+conn = sqlite3.connect('./data/authors.db')
+print(conn.execute("SELECT sql FROM sqlite_master WHERE type='table';").fetchall())
+conn.close()
--- process_raw_data.py
+++ process_raw_data.py
@@ -1,2 +1,34 @@
 """A script to process book data."""
+import pandas as pd
+import argparse
+import re
+import sys
+import csv
+import glob
 
+
+def get_command_line_arguments():
+    """Returns the given CLI Arguments."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'input_file',
+        help='Path to input file'
+    )
+    return parser.parse_args()
+
+
+def load_csv_file(input_file: str):
+    """ Loading the csv files to the database."""
+    data = pd.read_csv(input_file)
+    return data
+
+
+def clean_title():
+    """Remove content in brackets."""
+    pass
+
+
+if __name__ == "__main__":
+    args = get_command_line_arguments()
+    raw_data = load_csv_file(args.input_file)
+    print(raw_data.head())
